27c6678b156e34569d619625ccb91303eacc8a11
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-07-07 23:34:21 -0400

    First 4 sushi are rendering

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,25 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    allSushi: [],
+    selectedSushi: {}
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(sushiJSON => {
+      this.setState({
+        allSushi: sushiJSON.slice(0, 4)
+      })
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer allSushi={this.state.allSushi} />
         <Table />
       </div>
     );
